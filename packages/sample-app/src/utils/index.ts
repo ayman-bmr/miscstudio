17682def0da45e93e81ca2a214af225e1a7844af
ayman-bmr <aymanbouamar2004@gmail.com>
2025-09-12 15:53:55 +0100

    Update 1

--- packages/sample-app/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import i18n from "@repo/providers/i18n/i18n";
-
-export const getTranslateProperty = (obj: any, property: string): string => {
-  return obj[property + (i18n.language === "ar" ? "_ar" : "")];
-};
--- packages/sample-app/src/utils/urls.ts
+++
@@ -1,7 +0,0 @@
-export const getParamFromUrl = (key: string): string | null => {
-  if (typeof window !== "undefined") {
-    const params: URLSearchParams = new URLSearchParams(window.location.search);
-    return params.get(key);
-  }
-  return null;
-};
