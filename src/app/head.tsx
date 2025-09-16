5faa806a76be2deabc9bbd2f5a2afc2821004b87
ayman-bmr <aymanbouamar2004@gmail.com>
2025-09-16 08:59:38 +0100

    head update

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>misc studio</title>
+      <meta name="description" content="Welcome to Misc Studio" />
+      <link rel="icon" href="/icon-256-v2" />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-  "use client"
+
 
 import Hikayapage from "./components/hikayapage";
 import { Box } from "@mui/material";
@@ -7,11 +7,22 @@ import MiscStudioHero from "./components/homepage";
 import OurGamesSection from "./components/gamesSection";
 import Footer from "./components/footer";
 import AboutUsSection from "./components/AboutUs";
+import Head from "next/head";
+
+export const metadata = {
+  title: "Misc Studio",
+  description: "Welcome to miscstudio",
+  icons: {
+    icon: "/icon-256-v2.png",
+  },
+};
+
 
 
 
 export default function Home() {
   return (
+    <>
     <Box>
       <Header/>
        <MiscStudioHero />
@@ -19,6 +30,10 @@ export default function Home() {
        <AboutUsSection/>
         <Footer/>
     </Box>
+  
+    </>
+    
+    
     
    
     
